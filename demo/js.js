195fd81b782628036bcef41b3ec76837108827f5
Corey Hart <corey@codenothing.com>
2011-01-28 09:37:02 -0800

    -Adding allow comments option

--- demo/js.js
+++ demo/js.js
@@ -4,22 +4,24 @@
  * Corey Hart @ http://www.codenothing.com
  */
 jQuery(function(){
-	var textarea = jQuery('textarea').linedtextarea(), wrapper = jQuery('.linedwrap'), results = jQuery('#results'), lint;
+	var textarea = jQuery('textarea').linedtextarea(),
+		wrapper = jQuery('.linedwrap'),
+		results = jQuery('#results'),
+		comments = jQuery('input[type=checkbox]')[ 0 ],
+		lint;
 
 	jQuery('button').click(function(){
 		wrapper.removeClass('error').removeClass('success');
 		results.removeClass('error').removeClass('success');
-		lint = JSONLint( textarea.val() );
+		lint = JSONLint( textarea.val(), { comments: comments.checked } );
 
 		if ( ! lint.error ) {
-			results.html( 'Valid JSON.' );
 			wrapper.addClass('success');
-			results.addClass('success');
+			results.addClass('success').html( 'Valid JSON.' );
 		}
 		else {
 			wrapper.addClass('error');
-			results.addClass('error');
-			results.html([
+			results.addClass('error').html([
 				lint.error + "<br>" +
 				"<b>Line:</b> " + lint.line + "<br>" +
 				"<b>Character:</b> " + lint.character
