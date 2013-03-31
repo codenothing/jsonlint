760ce14dcb18c65fb1b2c2e137529cefec461496
Corey Hart <corey@codenothing.com>
2013-03-31 16:37:32 -0700

    -Upgrading munit package to run test suite on multiple node versions

--- Makefile
+++ Makefile
@@ -3,8 +3,14 @@
 all:
 	@echo "\n\x1B[1;31mPC_LOAD_LETTER\x1B[0m\n"
 
-test: lint
-	@node build/test.js
+clean:
+	@rm -rf build/results/
 
 lint:
 	@node build/lint.js
+
+test: clean lint
+	@node build/test.js
+
+test-full:
+	@./build/full.sh
--- build/test.js
+++ build/test.js
@@ -1,4 +1,7 @@
 global.munit = require( 'munit' );
 global.JSONLint = require( '../jsonlint.js' );
 
-munit.render( __dirname + '/../test/', { junit: __dirname + '/results/' } );
+munit.render( __dirname + '/../test/', {
+	junit: __dirname + '/results/',
+	junitPrefix: process.version.replace( /\./g, '_' )
+});
