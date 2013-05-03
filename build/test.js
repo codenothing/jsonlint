64339abc7b9fc1202c38b2dd069a019019567a51
Corey Hart <corey@codenothing.com>
2013-05-02 21:16:59 -0700

    -Upgrading munit package

--- build/test.js
+++ build/test.js
@@ -1,6 +1,11 @@
 global.munit = require( 'munit' );
 global.JSONLint = require( '../jsonlint.js' );
 
+// Only stop test suite when running make test
+if ( ! process.env.NODE_TEST_NO_SKIP ) {
+	munit.defaults.settings.stopOnFail = true;
+}
+
 munit.render( __dirname + '/../test/', {
 	junit: __dirname + '/results/',
 	junitPrefix: process.version.replace( /\./g, '_' )
